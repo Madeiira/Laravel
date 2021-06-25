b0c545c04e0763923078c5e61836a7ec73bf7069
Gabriel Anjos <gabriel.anjos@businessintegrator.com.br>
2021-06-25 18:10:11 -0300

    Add test View and Route

---
+++ hdcevents/resources/views/contactform.blade.php
@@ -0,0 +1,28 @@
+<html>
+<head><meta charset="UTF-8"></head>
+<form action="" method=""> 
+ <input type="hidden" name="recipient" value="exemplo@gmail.com"> 
+ <input type="hidden" name="redirect" value="http://seudominio."> 
+ <input type="hidden" name="subject" value="Assunto"> 
+ <input type="hidden" name="email" value="email@doseudominio">  
+ <p> 
+ Nome:<br /> 
+ <input type="text" size="30" name="nome"> 
+ </p>   
+ 
+ <p>
+ E-mail:<br /> 
+ <input type="text" size="30" name="replyto"> 
+ </p>   
+ 
+ <p> 
+ Comentários:<br /> 
+ <input type="text" size="35" name="Comentarios"> 
+ </p>   
+ 
+ <p> 
+ <input type="submit" name="BTEnvia" value="Enviar"> 
+ <input type="reset" name="BTApaga" value="Apagar"> </p> 
+ </form>
+ 
+</html> 
