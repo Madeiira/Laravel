39baffc35edb5046640e3a24859617010c0a4acf
Gabriel Anjos <gabriel.anjos@businessintegrator.com.br>
2021-06-27 19:17:47 -0300

    Front-end of the project

--- hdcevents/app/Http/Controllers/EventController.php
+++ hdcevents/app/Http/Controllers/EventController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Events;
 
 class EventController extends Controller
 {
     public function index(){
-        return view('welcome');
+        $events = Events::all();
+        return view('welcome', 
+        [
+            'events' => $events
+        ]);
     }
 
     public function create(){
